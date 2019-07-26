d3c44370f2b757dfe873e6820e7fd429506a1632
Ryan O'Rourke <rorourk2@binghamton.edu>
2019-07-26 16:34:35 -0400

    done

--- app/src/main/java/com/example/finalgame/Instructions.java
+++ app/src/main/java/com/example/finalgame/Instructions.java
@@ -1,11 +1,15 @@
 package com.example.finalgame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Instructions extends AppCompatActivity {
 
@@ -13,17 +17,47 @@ public class Instructions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
+
+        TextView text=(TextView)findViewById(R.id.textView12);
+        text.setText(Html.fromHtml("<h1>Instructions</h1>" +
+                "<br>" +
+                "<h2>The goal of the game is to keep your character alive for as long as possible.</h2>" +
+                "<br>" +
+                "<h2>Click where you want your character to go on the screen so they can avoid the oncoming dangers!</h2>" +
+                "<br>" +
+                "<h2>Good Luck!</h2>" +
+                "<br>" +
+                "<h2>And Have Fun!</h2>"));
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button s=findViewById(R.id.button);
+        Button s2=findViewById(R.id.button2);
+        s.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startLeaderActivity();
             }
         });
+        s2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startMainActivity();
+            }
+        });
+
+    }
+
+    public void startMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        //intent.putExtra("the",x);
+        startActivity(intent);
+    }
+    public void startLeaderActivity(){
+        Intent intent = new Intent(this, LearderBoard.class);
+        //intent.putExtra("the",x);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/finalgame/MainActivity.java
+++ app/src/main/java/com/example/finalgame/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.finalgame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +18,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button s=findViewById(R.id.button);
+        Button s2=findViewById(R.id.button2);
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        s.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startInstructActivity();
             }
         });
+        s2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startLeaderActivity();
+            }
+        });
+
     }
 
     @Override
@@ -48,5 +60,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void startInstructActivity(){
+        Intent intent = new Intent(this, Instructions.class);
+        //intent.putExtra("the",x);
+        startActivity(intent);
+    }
+    public void startLeaderActivity(){
+        Intent intent = new Intent(this, LearderBoard.class);
+        //intent.putExtra("the",x);
+        startActivity(intent);
     }
 }
